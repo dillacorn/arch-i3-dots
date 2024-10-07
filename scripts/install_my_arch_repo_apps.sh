2f61b859218e60e1b33a8c1ae90f125f0afddaca
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-10-07 11:59:24 -0400

    fix

--- scripts/install_my_arch_repo_apps.sh
+++ scripts/install_my_arch_repo_apps.sh
@@ -37,7 +37,7 @@ if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
 
     # Install the applications using pacman (list format)
     sudo pacman -S --needed --noconfirm \
-        i3 \
+        i3-wm \
         ttf-font-awesome \
         i3status \
         i3lock \
