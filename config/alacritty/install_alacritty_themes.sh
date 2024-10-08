4e1d66984bbabbddfbca60f4795cc15e8d8f0bbf
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-10-08 02:43:38 +0000

    Update install_alacritty_themes.sh

--- config/alacritty/install_alacritty_themes.sh
+++ config/alacritty/install_alacritty_themes.sh
@@ -3,8 +3,19 @@
 # make executable:
 # chmod +x install_alacritty_themes.sh
 
-git clone https://github.com/alacritty/alacritty-theme
-mv alacritty-theme themes
+# Attempt to clone the repo, but don't fail the script if the directory already exists
+if git clone https://github.com/alacritty/alacritty-theme; then
+    echo "Repository cloned successfully."
+else
+    echo "Warning: alacritty-theme directory already exists or could not be cloned."
+fi
+
+# Move the directory only if it doesn't already exist
+if [ ! -d "themes" ]; then
+    mv alacritty-theme themes
+else
+    echo "Warning: 'themes' directory already exists and was not overwritten."
+fi
 
 # run command:
 # ./install_alacritty_themes.sh
