2bbe5e4afed82eff9a5f9251419258f882ccad32
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-10-09 15:37:07 -0400

    Update custom_res.sh
    Installed xf86-video-amdgpu driver so this configuration changes.. Instead of DP-2 or for example it's DisplayPort-2

--- config/i3/scripts/custom_res.sh
+++ config/i3/scripts/custom_res.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # turn off CRT
-xrandr --output DP-3 --off
+xrandr --output DisplayPort-2 --off
 
 # get modeline string for xrandr using gtf and/or cvt 
 # [CRT ~ Compaq FS7550]
@@ -30,23 +30,23 @@ xrandr --newmode "1352x1080_240.00"  571.31  1352 1480 1632 1912  1080 1081 1084
 
 # apply newmode to specific output
 # [CRT ~ Compaq FS7550]
-xrandr --addmode DP-3 "1024x768_105.39"
-xrandr --addmode DP-3 "1152x864_94.30"
-xrandr --addmode DP-3 "1344x1005_81.67"
-xrandr --addmode DP-3 "1600x1200_68.91"
+xrandr --addmode DisplayPort-2 "1024x768_105.39"
+xrandr --addmode DisplayPort-2 "1152x864_94.30"
+xrandr --addmode DisplayPort-2 "1344x1005_81.67"
+xrandr --addmode DisplayPort-2 "1600x1200_68.91"
 
 # apply newmode to specific output
 # [IPS 1080p 240hz ~ ViewSonic XG2431]
-xrandr --addmode DP-2 "1400x1050_240.00"
-xrandr --addmode DP-2 "1352x1080_240.00"
+xrandr --addmode DisplayPort-1 "1400x1050_240.00"
+xrandr --addmode DisplayPort-1 "1352x1080_240.00"
 
 # apply output mode to CRT
-# xrandr --output DP-3 --mode "1024x768_105.39" --pos 0x0 --rotate normal
-# xrandr --output DP-3 --mode "1152x864_94.30" --pos 0x0 --rotate normal
-# xrandr --output DP-3 --mode "1344x1005_81.67" --pos 0x0 --rotate normal
-# xrandr --output DP-3 --mode "1600x1200_68.91" --pos 0x0 --roate normal
+# xrandr --output DisplayPort-2 --mode "1024x768_105.39" --pos 0x0 --rotate normal
+# xrandr --output DisplayPort-2 --mode "1152x864_94.30" --pos 0x0 --rotate normal
+# xrandr --output DisplayPort-2 --mode "1344x1005_81.67" --pos 0x0 --rotate normal
+# xrandr --output DisplayPort-2 --mode "1600x1200_68.91" --pos 0x0 --roate normal
 
 # additional xrandr settings here
 # ViewSonic XG2431 (capped to 94hz to reduce crosstalk)
-xrandr --output DP-2 --mode 1920x1080 --rate 240 --pos 0x0 --rotate normal
+xrandr --output DisplayPort-1 --mode 1920x1080 --rate 240 --pos 0x0 --rotate normal
 # adjust "--pos 1024x0, 1152x0, 1344x0, 1600x0" depending on CRT configuration
