5b20203dc374ecfec0438d3fd0bd4f93dfc9ad05
dillacorn <28743863+dillacorn@users.noreply.github.com>
2024-10-22 19:33:58 -0400

    Update install_my_arch_repo_apps.sh

--- scripts/install_my_arch_repo_apps.sh
+++ scripts/install_my_arch_repo_apps.sh
@@ -69,7 +69,7 @@ if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
     # Terminal Applications
     # ----------------------------
     echo -e "${CYAN}Installing terminal applications...${NC}"
-    for pkg in micro alacritty fastfetch btop htop curl wget git dos2unix brightnessctl ipcalc cmatrix sl asciiquarium figlet; do
+    for pkg in micro alacritty fastfetch btop htop curl wget git dos2unix brightnessctl ipcalc cmatrix sl asciiquarium figlet python-openai-whisper python-pyaudio portaudio; do
         install_package "$pkg"
     done
 
